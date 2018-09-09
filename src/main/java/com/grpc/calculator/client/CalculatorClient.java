fb21f62cb099d6b653af1d2be0591f6fa0955071
devson <sonyc5720@gmail.com>
2018-09-09 22:52:21 +0900

    average 호출하는 client 코드 추가

--- src/main/java/com/grpc/calculator/client/CalculatorClient.java
+++ src/main/java/com/grpc/calculator/client/CalculatorClient.java
@@ -3,13 +3,17 @@ package com.grpc.calculator.client;
 import com.grpc.calculator.*;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
 
 import java.util.Iterator;
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 public class CalculatorClient {
 
-    public static void main(String[] args) {
-        runFactorization(2394);
+    public static void main(String[] args) throws Exception {
+        runAverage();
     }
 
     private static ManagedChannel getChannel() {
@@ -20,6 +24,10 @@ public class CalculatorClient {
 
     private static CalculatorServiceGrpc.CalculatorServiceBlockingStub getBlockingStub(ManagedChannel channel) {
         return CalculatorServiceGrpc.newBlockingStub(channel);
+    }
+
+    private static CalculatorServiceGrpc.CalculatorServiceStub getAsyncStub(ManagedChannel channel) {
+        return CalculatorServiceGrpc.newStub(channel);
     }
 
     private static void runAdd(int num1, int num2) {
@@ -61,6 +69,59 @@ public class CalculatorClient {
 
         channel.shutdown();
         System.out.println("\ngRPC client shutdown");
+    }
+
+    private static void runAverage() throws InterruptedException {
+        ManagedChannel channel = getChannel();
+        CalculatorServiceGrpc.CalculatorServiceStub asyncStub = getAsyncStub(channel);
+
+        System.out.println("gRPC client started");
+
+        CountDownLatch latch = new CountDownLatch(1);
+
+        StreamObserver<AverageRequest> requestStreamObserver = asyncStub.average(new StreamObserver<AverageResponse>() {
+            // server로부터 response가 왔을 때 (여기서는 평균값을 한번 주기 때문에 한번만 호출)
+            @Override
+            public void onNext(AverageResponse value) {
+                System.out.println(" = " + value.getResult());
+            }
+
+            // server로부터 error를 받았을 때
+            @Override
+            public void onError(Throwable t) {
+                t.printStackTrace();
+            }
+
+            // server가 모든 response를 보냈음을 알렸을 때 (server에서 onCompleted() 호출)
+            @Override
+            public void onCompleted() {
+                latch.countDown();
+                System.out.println("response complete");
+            }
+        });
+
+        Random random = new Random();
+
+        for (int i = 0; i < 10; i++) {
+            int randomNum = random.nextInt(100);
+            System.out.print(" + " + randomNum);
+
+            AverageRequest request = AverageRequest.newBuilder()
+                                                    .setReqNum(randomNum)
+                                                    .build();
+
+            // server에 request를 보냄
+            requestStreamObserver.onNext(request);
+        }
+
+        // server에게 request를 모두 보냈음을 알림
+        requestStreamObserver.onCompleted();
+
+        // count가 0이 될때까지 기다린다
+        latch.await(3, TimeUnit.SECONDS);
+
+        channel.shutdown();
+        System.out.println("client shutdown");
     }
 
 }
