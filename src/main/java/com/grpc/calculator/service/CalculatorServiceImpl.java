429adf5a5d2a7735fccc3142d656a9767c96a95d
devson <sonyc5720@gmail.com>
2018-09-09 22:02:49 +0900

    factorization 메서드 구현

--- src/main/java/com/grpc/calculator/service/CalculatorServiceImpl.java
+++ src/main/java/com/grpc/calculator/service/CalculatorServiceImpl.java
@@ -1,9 +1,6 @@
 package com.grpc.calculator.service;
 
-import com.grpc.calculator.AddRequest;
-import com.grpc.calculator.AddResponse;
-import com.grpc.calculator.Calculator;
-import com.grpc.calculator.CalculatorServiceGrpc;
+import com.grpc.calculator.*;
 import io.grpc.stub.StreamObserver;
 
 public class CalculatorServiceImpl extends CalculatorServiceGrpc.CalculatorServiceImplBase {
@@ -15,6 +12,30 @@ public class CalculatorServiceImpl extends CalculatorServiceGrpc.CalculatorServi
         responseObserver.onNext(AddResponse.newBuilder()
                                             .setResult(result)
                                             .build());
+
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void factorization(FactorizationRequest request, StreamObserver<FactorizationResponse> responseObserver) {
+        int reqNum = request.getReqNum();
+        int k = 2;
+
+        while (reqNum > 1) {
+
+            if (reqNum % k == 0) {
+                reqNum /= k;
+
+                FactorizationResponse response = FactorizationResponse.newBuilder()
+                                                                        .setResult(k)
+                                                                        .build();
+
+                responseObserver.onNext(response);
+            } else {
+                k++;
+            }
+
+        }
 
         responseObserver.onCompleted();
     }
