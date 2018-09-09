46ba0c870b8755743021cdc39e4537964669ccfc
devson <sonyc5720@gmail.com>
2018-09-09 22:52:03 +0900

    average 메서드 구현

--- src/main/java/com/grpc/calculator/service/CalculatorServiceImpl.java
+++ src/main/java/com/grpc/calculator/service/CalculatorServiceImpl.java
@@ -39,4 +39,37 @@ public class CalculatorServiceImpl extends CalculatorServiceGrpc.CalculatorServi
 
         responseObserver.onCompleted();
     }
+
+    @Override
+    public StreamObserver<AverageRequest> average(StreamObserver<AverageResponse> responseObserver) {
+        StreamObserver<AverageRequest> requestStreamObserver = new StreamObserver<AverageRequest>() {
+
+            int sum = 0;
+            int counter = 0;
+
+            // client로부터 request가 들어왔을 때
+            @Override
+            public void onNext(AverageRequest value) {
+                sum += value.getReqNum();
+                counter++;
+            }
+
+            // client로부터 error를 받았을 때
+            @Override
+            public void onError(Throwable t) {
+                t.printStackTrace();
+            }
+
+            // client가 모든 request를 보냈음을 알렸을 때 (client에서 onCompleted() 호출)
+            @Override
+            public void onCompleted() {
+                responseObserver.onNext(AverageResponse.newBuilder()
+                                                        .setResult((double)sum / counter)
+                                                        .build());
+                responseObserver.onCompleted();
+            }
+        };
+
+        return requestStreamObserver;
+    }
 }
